2e70953dd837af9718a62274dcff52a83d42c50a
Unknown <AG-Systems@users.noreply.github.com>
2015-06-28 17:52:02 -0700

    Create rbotv1.py

---
+++ rbotv1.py
@@ -0,0 +1,16 @@
+import praw
+import time
+from praw.helpers import comment_stream
+
+r = praw.Reddit("Just a regular bot.")
+r.login()
+
+target_text = "aurigabot"
+response_text = "Auriga is here!! Do you want to message my creater? [Click right here!](http://reddit.com/u/smarttechadvice)"
+
+processed = []
+while True:
+    for c in comment_stream(r, 'sandboxtest'):
+        if target_text in c.body and c.id not in processed:
+            c.reply(response_text)
+            processed.append(c.id)
