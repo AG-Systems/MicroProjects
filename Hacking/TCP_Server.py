cb45c69afb41072285acee032b6f8872174b0b21
Unknown <AG-Systems@users.noreply.github.com>
2015-08-31 01:05:47 -0700

    Create TCP_Server.py

---
+++ Hacking/TCP_Server.py
@@ -0,0 +1,34 @@
+import socket
+import threading
+
+bind_ip = "0.0.0.0"
+bind_port = 9999
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+server.bind((bind_ip,bind_port))
+
+server.listen
+
+print "[*] Listening on %s:%d" % (bind_ip, bind_port)
+
+def handle_client(client_socket):
+
+    request = client_socket.recv(1024)
+
+    print "[*] Received: %s" % request
+
+    client_socket.send("Meepo!")
+
+    client_socket.close()
+
+while True:
+
+    client,addr = server.accept()
+
+    print "[*] Accepted connection from: $s:%d" % (addr[0], addr[1])
+    #Having trouble the line above me
+    client_handler = threading.Thread(target=handle_client, args=(client))
+    client_handler.start()
+
+
