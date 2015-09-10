762e2256b269a2057e33c8f44e9582c01027d5c1
Unknown <AG-Systems@users.noreply.github.com>
2015-09-10 13:05:29 -0700

    Create ItemDatabase.cs

---
+++ GameScripts/ItemDatabase.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ItemDatabase : MonoBehaviour 
+{
+
+	public List<Item> items = new List<Item>();
+
+	void Start()
+	{
+		items.Add (new Item ());
+	}
+
+}
