6d4d7f4bd8fa3348d62b427a07ac45211810423a
Unknown <AG-Systems@users.noreply.github.com>
2015-09-14 13:05:59 -0700

    Create item1.cs

---
+++ GameScripts/item1.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using System.Collections;
+
+public class item1 : MonoBehaviour
+{
+	
+	void OnGUI()
+	{
+		if(render)
+		{
+			// Make a background box
+			GUI.Box(new Rect(25, 40, 70, 70), " ");
+		}
+	}
+	
+	private bool render = false;
+	private Rect windowRect = new Rect (20, 20, 120, 50);
+	
+	public void ShowWindow() {
+		render = true;
+	}
+	
+	public void HideWindow() {
+		render = false;
+	}
+	
+	public void Update() 
+	{
+		if (Input.GetKey(KeyCode.E))
+			ShowWindow();
+		
+		if (Input.GetKey(KeyCode.Q))
+			HideWindow();
+		
+	}
+}
+
