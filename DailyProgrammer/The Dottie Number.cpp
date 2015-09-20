9627fdd83b4ec5d0dffcb48eaf61f360b411215e
Unknown <AG-Systems@users.noreply.github.com>
2015-09-20 01:34:10 -0700

    Create The Dottie Number.cpp

---
+++ DailyProgrammer/The Dottie Number.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cmath>
+#include <math.h>
+
+int main()
+{
+	double num;
+	for ( int x = 0; x < 100; x++)
+	{
+	
+	num = cos(x);
+	std::cout << num << std::endl;
+	if (num = 0.74)
+	{
+		std::cout << "The number cos for Dottie number is: " << x << std::endl;
+		break;
+	}
+
+	}
+}
