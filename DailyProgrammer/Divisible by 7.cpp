c902fe6d0e0ebb9b3d3055c51eadfe1b5fb90f08
Unknown <AG-Systems@users.noreply.github.com>
2015-09-23 00:05:12 -0700

    Create Divisible by 7.cpp

---
+++ DailyProgrammer/Divisible by 7.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+
+
+int main() 
+{
+int number, reverse = 0;
+//std::cout<< "Input a Number to Reverse and press Enter: ";
+// std::cin>> number;
+int n = 5432;
+std::cout << n << std::endl;
+number = n;
+
+   for( ; number!= 0 ; )
+   {
+      reverse = reverse * 10;
+      reverse = reverse + number%10;
+      number = number/10;
+   }
+std::cout<< "Reversed Number is:  "<<reverse;
+   for( int z = 0; z < 20; z++)
+   {
+	std:: cout << number << reverse;
+	   if ( number % 7)
+		{
+			std:: cout << number << reverse;
+			if ( reverse % 7)
+			{
+			std::cout << number << reverse;
+			}
+		}
+   }
+
+ 
+   return 0;
+   system("PAUSE");
+}
