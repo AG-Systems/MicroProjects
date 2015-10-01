b6dc3e0023816444ed3cebb1ec764adcd45fba0f
Unknown <AG-Systems@users.noreply.github.com>
2015-10-01 13:13:35 -0700

    Update temp.cpp

--- PROJECT/BlackJack/cpp/temp.cpp
+++ PROJECT/BlackJack/cpp/temp.cpp
@@ -1,30 +1,128 @@
 #include <iostream>
-#include <time.h>
 #include <stdlib.h>
+#include <time.h>
+#include <string>
 
-const char arrayNum[11] = {'1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '10'};
-int CardDeal()
+
+int HitMe()
 {
-	int Rand1 = rand() % 11; 
-	int Rand2 = rand() % 11; 
+    rand() % 11;   
 }
 
-int YouLose()
+int Total = 21;
+
+void YouLost()
 {
-	std::cout << "You lose. Your total was: " << std::endl;
+    std::cout << "You lost bro gg wp " << std::endl;   
+
+}
+void YouFold()
+{
+    std::cout << "You folded " << std::endl; 
+
 }
 
-int main()
+void YouWon()
 {
-	int CardDeal();
-	int Rand1 = rand() % 11; 
-	int Rand2 = rand() % 11; 
+    std::cout << " You won the game. You hit 21 " << std::endl; 
+}
 
-	std::cout << "Welcome to blackjack meeps " << std::endl;
-	std::cout << "Press any key to start " << std::endl;
-	std::cin.get();
-	std::cout << "Card 1 is : " << Rand1 << "\n " << "Card 2 is : " << Rand2 << std::endl;
-	std::cout << "Here is your total: " << CardDeal() << std::endl;
+
+int main ()
+{
+  srand ( time(NULL) ); //initialize the random seed
+  
+
+  const char arrayNum[11] = {'1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '10'};
+  int RandIndex = rand() % 11; 
+  int Rand2 = rand() % 11; 
+  if (int RandIndex = 0 )
+  {
+	RandIndex = rand() % 11; 
+  }
+  std::cout << "Card 1: " << RandIndex << "\n" << "Card 2: " << Rand2 << std::endl;
+  
+  int RandCard2 = Rand2 + RandIndex;
+  
+  std::cout << "Your total is " << RandCard2 << std::endl;
+  
+  std::string input;
+  
+  std::cout << "Do you want to hit some more or fold? " << std::endl;
+  
+  
+  
+  int HitMeInt = 0;
+  
+  int RandFinal = RandIndex + Rand2;
+  
+for (int loops = -1; loops < HitMeInt;)
+{
+  if ( RandFinal < Total)
+    {
+    std::cin >> input;
+    if ( RandFinal < 21)
+    {
+    
+    if(input == "hitme")
+        {
+            HitMeInt++;
+            int NewCard = ( rand() % 11 ) * 1;
+            if( HitMeInt == 2)
+            {
+                int NewCard2 = NewCard + (rand() % 11);
+                RandFinal = NewCard2 + RandIndex + Rand2;
+                std::cout << "Your new Card is: " << NewCard2 << std::endl;
+
+                std::cout << "Your New total is: " << RandFinal << std::endl;
+            }
+            
+            // std::cout << HitMeInt << std::endl;
+            if ( HitMeInt == 1)
+            {
+            int NewCard = ( rand() % 11 ) * 1;  
+            RandFinal = NewCard + RandIndex + Rand2;
+            std::cout << "Your new Card is: " << NewCard << std::endl;
+            std::cout << "Your New total is: " << RandFinal << std::endl;
+            }
+
+            if ( RandFinal > 21)
+            {
+                std::cout << "Your final is: " << RandFinal << std::endl;
+                YouLost(); 
+                break;
+            }
+
+        }
+    }
+    if ( RandFinal > 21)
+    {
+        std::cout << "Your final is: " << RandFinal << std::endl;
+        YouLost(); 
+        break;
+    }
+    
+    if ( RandFinal == 21)
+    {
+        YouWon();
+        break;
+    }
+
+    }
+        if(input == "fold")
+        {
+          int RandFinal = RandIndex + Rand2;
+          std::cout << RandFinal << std::endl;  
+            YouFold();
+        }
+
+
+
+  // std::cout << arrayNum[RandIndex] << std::endl;
+  system("PAUSE");
+}
+}
+
+
+
 	
-
-}
