90bdeccae82322914b73c2fc6839cd48951847e1
Unknown <AG-Systems@users.noreply.github.com>
2015-10-02 13:14:24 -0700

    Create BlackJack.py

---
+++ PROJECT/BlackJack/py/BlackJack.py
@@ -0,0 +1,26 @@
+import random
+print ( " Welcome to BlackJack ")
+card1 = (random.randint(2,11))
+card2 = (random.randint(2,11))
+card3 = (random.randint(2,11))
+card4 = (random.randint(2,11))
+card5 = (random.randint(2,11))
+
+print ( "Your card 1 is : " )
+
+print card1
+
+print ( " Your card 2 is : ")
+
+print card2
+
+hitme = input("Hitme or fold ")
+
+if input = hitme:
+	
+	
+	
+
+
+
+
