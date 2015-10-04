87fb866a9d6d3aa93dd8b6faccb4014c8581c712
Unknown <AG-Systems@users.noreply.github.com>
2015-10-04 15:32:38 -0700

    Create MonitorShutOff.cpp

---
+++ Templates/MonitorShutOff.cpp
@@ -0,0 +1,8 @@
+#include<Windows.h>
+
+int main()
+{
+SendMessage(HWND_BROADCAST, WM_SYSCOMMAND, SC_MONITORPOWER, (LPARAM)2);
+Sleep(5000);
+SendMessage(HWND_BROADCAST, WM_SYSCOMMAND, SC_MONITORPOWER, (LPARAM)-1);
+}
