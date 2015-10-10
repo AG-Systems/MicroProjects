78305c0fa9f8074ab8d745d3eeb4bc10fb268d07
Unknown <AG-Systems@users.noreply.github.com>
2015-10-10 02:33:48 -0700

    Update RoryFinal.py

--- Encryption/RoryFinal.py
+++ Encryption/RoryFinal.py
@@ -22,3 +22,15 @@ with open(root.fileName, "rb") as imageFile:
     f.write(HashImage)
     f.close()
     
+
+#line comments:
+# 1-4 All of the modules. Typical stuff
+# 5-6 Makes a window then opens a file dialouge
+# 10 Just displays the name of the file and the directory.
+# 14 - 23 Opens the file, then converts it into bytes and places it into a string. 
+# Prints out the type then decodes it into standard text. Prints out the hash number from the hashlib
+# Opens a text file then appeands and writes the hash value. 
+# This program is not ment to be taken too seriously as there is WAYYYYYYYY better alternatives.  I am very proud of what I came up with
+# Works best with .jpeg for now.
+# Finished: Oct 10, 2015, 1 am.
+# Auther: AG-Systems
