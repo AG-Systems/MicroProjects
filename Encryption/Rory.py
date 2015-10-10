e0875167e02de6e0fd4fea2e31db16a3c74ad4f5
Unknown <AG-Systems@users.noreply.github.com>
2015-10-09 23:47:57 -0700

    Update Rory.py

--- Encryption/Rory.py
+++ Encryption/Rory.py
@@ -2,6 +2,7 @@ from tkinter import *
 import os
 import base64
 import hashlib
+import subprocess
 root = Tk()
 root.fileName = filedialog.askopenfilename( filetypes = ( ("Normal files", "*.jpeg*"),("All files", "*.*")))
 
@@ -13,13 +14,29 @@ print (root.fileName)
 
 with open(root.fileName, "rb") as imageFile:
     str = base64.b64encode(imageFile.read())
+
+
     #print (str)
+    
+    base64.b64encode(b'your name')
+    base64.b64encode('your name'.encode('ascii'))
+    print(hashlib.md5('your name'.encode('utf-8')).hexdigest())
+    #print(hashlib.md5(str.encode('utf-8')).hexdigest())
+    
     #if you print this string out you will get a huge mess
 
-#print(hashlib.md5(str.encode('utf-8')).hexdigest())
 
+comment2 = """
+f = open("imgData.txt","a") #opens file with name of "imgData.txt"
+f.write(str)
+f.close()
+print(hashlib.md5(str.encode('ansii')).hexdigest())
+"""
 
 comment1 = """ fh = open("imageToSave.png", "wb")
 fh.write(str.decode('base64'))
 fh.close()
 """
+
+
+
