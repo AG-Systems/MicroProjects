8a80c0b4e5e52dec7dbdb52b73b270b8eb5fa269
Auriga <AG-Systems@users.noreply.github.com>
2015-10-13 11:31:15 -0700

    Update SimpleXOR.cpp

--- Encryption/SimpleXOR.cpp
+++ Encryption/SimpleXOR.cpp
@@ -29,3 +29,12 @@ int main()
 	return 0;
 	
 }
+
+/*
+
+Line 1-4: Including the generel modules. Fstream and windows are not needed.
+
+Line 13: Starts the for loop.  Runs how big the original string size is.
+Line 15: += is a compound which adds to the general string of enc and whatever character the str is at. The ^ symbol makes it so bitwise exclusive or the char key + i divided by 20. 
+
+line 23: make sure the dencrypted string adds and equals to the enc with the interger i equal to OR equal to the char key + i divided by 20;
