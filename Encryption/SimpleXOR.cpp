e70f34aadb804bf8fb7c4cc6779daedfa23689be
Auriga <AG-Systems@users.noreply.github.com>
2015-10-14 10:44:30 -0700

    Update SimpleXOR.cpp

--- Encryption/SimpleXOR.cpp
+++ Encryption/SimpleXOR.cpp
@@ -38,3 +38,5 @@ Line 13: Starts the for loop.  Runs how big the original string size is.
 Line 15: += is a compound which adds to the general string of enc and whatever character the str is at. The ^ symbol makes it so bitwise exclusive or the char key + i divided by 20. 
 
 line 23: make sure the dencrypted string adds and equals to the enc with the interger i equal to OR equal to the char key + i divided by 20;
+
+*/
