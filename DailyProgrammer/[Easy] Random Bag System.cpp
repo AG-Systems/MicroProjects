fb8cd5ebeab5d24c6f9a07cb6c5a143ff28412d6
Auriga <AG-Systems@users.noreply.github.com>
2015-10-20 13:07:33 -0700

    Update [Easy] Random Bag System.cpp

--- DailyProgrammer/[Easy] Random Bag System.cpp
+++ DailyProgrammer/[Easy] Random Bag System.cpp
@@ -1,9 +1,15 @@
-
+// Example program
 #include <iostream>
 #include <string>
+#include <time.h>
+#include <stdlib.h>
+
 
 int main()
 {
+    srand ( time(NULL) ); 
     std::string textArray[7] = {"O", "I", "S", "Z", "L", "J", "T"};
+    int RandIndex = rand() % 7; 
+    std::cout << textArray[RandIndex];
     
 }
