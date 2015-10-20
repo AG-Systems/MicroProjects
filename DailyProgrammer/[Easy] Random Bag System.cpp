f88a6b03d319fc34c4c88f15b4335912d98c4215
Auriga <AG-Systems@users.noreply.github.com>
2015-10-20 13:16:28 -0700

    Update [Easy] Random Bag System.cpp

--- DailyProgrammer/[Easy] Random Bag System.cpp
+++ DailyProgrammer/[Easy] Random Bag System.cpp
@@ -13,3 +13,47 @@ int main()
     std::cout << textArray[RandIndex];
     
 }
+
+/*
+CREDIT: https://github.com/iamtechi27/randombag/blob/master/randombag.cpp
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+Saving this for later.
+-----------------------------------
+
+#include <iostream>
+#include <cstdio>
+#include <ctime>
+#include <string>
+#include <cstdlib>
+using namespace std;
+
+string bag;
+
+void init() {
+	bag = "OISZLJT";
+} //init()
+
+char draw() {
+	int randomNumber;
+	char result;
+	if (bag.length() == 0) {
+		init();
+	} //if (bag.length() == 0)
+	randomNumber = rand() % bag.length();
+	result = bag.at(randomNumber);
+	bag.erase(bag.begin()+randomNumber);
+	return result;
+} //draw()
+
+int main() {
+	srand(time(NULL));
+	for (int i = 0; i < 50; i++) {
+		if (i % 7 == 0) {
+			cout << " ";
+		} //if (i % 7 == 0)
+		cout << draw();
+	} //for (int i < 50)
+	cout << endl;
+}
+
+*/
