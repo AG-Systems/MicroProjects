11d166462e412a77edd1e9033fcba93f8b7fc75f
Auriga <AG-Systems@users.noreply.github.com>
2015-10-22 08:28:48 -0700

    Create NewTemp.cpp

---
+++ MachineLearning/NewTemp.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <fstream>
+
+
+std::string input;
+
+const char AlphabetLower[26] =
+{
+	'a', 'b', 'c', 'd', 'e', 'f', 'g',
+	'h', 'i', 'j', 'k', 'l', 'm', 'n',
+	'o', 'p', 'q', 'r', 's', 't', 'u',
+	'v', 'w', 'x', 'y', 'z'
+};
+
+char counter [26] = 
+{  
+	'a', 'b', 'c', 'd', 'e', 'f', 'g',
+	'h', 'i', 'j', 'k', 'l', 'm', 'n',
+	'o', 'p', 'q', 'r', 's', 't', 'u',
+	'v', 'w', 'x', 'y', 'z'        
+}; 
+
+int main()
+{
+	std::cout << "Please start typing: " << std::endl;
+	std::cin >> input;
+	std::ofstream myfile;
+	myfile.open ("MachineLearning.txt");
+	int * num;
+	for(int z = 0; z < 26;z++)
+	{
+		size_t n = std::count(input.begin(), input.end(), AlphabetLower[z]);
+		std::cout << AlphabetLower[z] << n << std::endl;
+		myfile << AlphabetLower[z] << n << std::endl;
+		// counter[z] = n;
+	   	//std::cout << counter[z] << n << std::endl;
+	   	// Assign each elemet with the number n to make sure you are counting the times of a specific letter.
+
+	}
+
+
+}
