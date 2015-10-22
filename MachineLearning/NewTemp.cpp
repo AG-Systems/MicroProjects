d08fdd7bda1ec5b36024513818885b0bdb9cdb1c
Auriga <AG-Systems@users.noreply.github.com>
2015-10-22 09:32:04 -0700

    Update NewTemp.cpp

--- MachineLearning/NewTemp.cpp
+++ MachineLearning/NewTemp.cpp
@@ -32,8 +32,8 @@ int main()
 	for(int z = 0; z < 26;z++)
 	{
 		size_t n = std::count(input.begin(), input.end(), AlphabetLower[z]);
-		std::cout << AlphabetLower[z] << n << std::endl;
-		myfile << AlphabetLower[z] << n << std::endl;
+		std::cout << AlphabetLower[z] << ": " << n << std::endl;
+		myfile << AlphabetLower[z] << ": " << n << std::endl;
 		// counter[z] = n;
 	   	//std::cout << counter[z] << n << std::endl;
 	   	// Assign each elemet with the number n to make sure you are counting the times of a specific letter.
