e5a23538d21f43a835b5a975c1fd03954be75098
Auriga <AG-Systems@users.noreply.github.com>
2015-10-25 19:18:46 -0700

    Update Ver1.cpp

--- MachineLearning/markov-chain/Ver1.cpp
+++ MachineLearning/markov-chain/Ver1.cpp
@@ -1,1 +1,37 @@
+#include <sstream>
+#include <iostream>
+#include <stdlib.h>
+#include <time.h>
+#include <string>
 
+int main()
+{
+    srand ( time(NULL) );
+    std::string input = "test one two three.";	
+	int words = 0; // Holds number of words
+	
+	for(int i = 0; input[i] != '\0'; i++)
+	{
+		if (input[i] == ' ') //Checking for spaces
+		{
+			words++;
+		} 		
+	}
+    
+	std::cout << "The number of words = " << words+1 << std::endl;
+	int n = words+1;
+    std::string arr[n];
+    int i = 0;
+    std::stringstream ssin(input);
+    while (ssin.good() && i < 4)
+    {
+        ssin >> arr[i];
+        ++i;
+    }
+    for(i = 0; i < 4; i++)
+    {
+        std::cout << arr[i] << std::endl;
+    }
+    int RandIndex = rand() % n; 
+    std::cout << arr[RandIndex];
+}
