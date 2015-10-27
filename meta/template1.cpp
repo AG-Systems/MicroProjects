ea53add38efee426a5f9450bf130f4609d064c2a
Auriga <AG-Systems@users.noreply.github.com>
2015-10-27 12:39:29 -0700

    Create template1.cpp

---
+++ meta/template1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+template <typename Type> // this is the template parameter declaration
+Type max(Type tX, Type tY)
+{ 
+    std::cout << tX << std::endl;
+}
+
+int main()
+{
+  std::cout << max(Tpye tX, Type tY) << std::endl;
+  return 0;
+}
