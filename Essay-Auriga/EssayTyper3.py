0b7c924dbbc36bd10ca1532326d2de1d7498723a
Auriga <AG-Systems@users.noreply.github.com>
2015-11-01 21:42:50 -0800

    Create EssayTyper3.py

---
+++ Essay-Auriga/EssayTyper3.py
@@ -0,0 +1,32 @@
+import wikipedia
+import re
+import random
+
+testVar = raw_input("Please input the topic you want the computer write for you: ")
+print wikipedia.summary(testVar, sentences=2)
+str = wikipedia.summary(testVar, sentences=2)
+strf = str.split(" ")
+print(" \n ")
+
+text_file = open("EssayTyper.txt", "w")
+
+for x in range(0, len(str)):
+    chain = random.choice(strf)
+    if chain in strf:
+        x += 1
+        text_file.write(chain)
+        text_file.write(" ")
+        print(random.choice(strf))
+    #print(random.choice(strf))
+
+text_file.close()
+
+
+# Python 2.7
+# Must use pip install wikipedia
+
+
+
+
+
+
