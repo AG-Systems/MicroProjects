1355a4b142c8d4881ee90bf7e4659441277475eb
Auriga <AG-Systems@users.noreply.github.com>
2015-11-09 13:19:14 -0800

    Create bogosort.cpp

---
+++ Algorithm/bogosort.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <algorithm>
+#include <cstdlib>
+#include <vector>
+#include <algorithm>
+
+std::vector<int> list;
+
+int main()
+{
+    int a[8] = {0, 1, 2, 3, 4, 5, 6, 7};
+    std::srand(unsigned(time(0)));
+
+    std::random_shuffle(a, a + 8);
+
+    for (int i=0; i<8; i++)
+    {
+        std::cout << a[i] << " ";
+        list.push_back (a[i]);
+    }
+    
+    bool running = true
+    while(running)
+    {
+        if (is_sorted(list.begin(), list.end()) )
+        {
+            running = false;
+            break;
+        }
+       std::random_shuffle(list_.begin(), list_.end()); 
+    }
+    std::cout << "Finished " << std::endl;
+    
+}
