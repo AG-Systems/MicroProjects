65b5db676860efe7f889d67695fab095d684a412
Auriga <AG-Systems@users.noreply.github.com>
2015-11-17 22:26:36 -0800

    Update AutoFix.cpp

--- temp/AutoFix.cpp
+++ temp/AutoFix.cpp
@@ -2,19 +2,50 @@
 #include <string>
 #include <time.h>
 
+using namespace std;
+class timer {
+	private:
+		unsigned long begTime;
+	public:
+		void start() {
+			begTime = clock();
+		}
+
+		unsigned long elapsedTime() {
+			return ((unsigned long) clock() - begTime) / CLOCKS_PER_SEC;
+		}
+
+		bool isTimeout(unsigned long seconds) {
+			return seconds >= elapsedTime();
+		}
+};
+
 int main()
 {
     std::string sentence;
     char input;
     std::string dictionary[4] = {"the","problem","th"};
+    unsigned long seconds = 2;
+	timer t;
+
+	cout << seconds <<  " seconds elapsed" << endl;
     for(int z = 0; z < 1; z--)
     {
+        t.start();
         std::cin >> input;
+	    while(true) 
+	    {
+		if(t.elapsedTime() >= seconds) 
+		{
+			
+		}
+		else {
+            
+		    }
+	    }
         
-        if (input == "\n")
-        {
-            break;    
-        }
+
+        
         
     
 }
