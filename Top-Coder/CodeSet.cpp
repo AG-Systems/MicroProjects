d0a1b0f93ad3340ce328dc5475594f942e0b31d4
Auriga <AG-Systems@users.noreply.github.com>
2015-11-19 10:54:07 -0800

    Update CodeSet.cpp

--- Top-Coder/CodeSet.cpp
+++ Top-Coder/CodeSet.cpp
@@ -1,16 +1,43 @@
 #include <iostream>
 #include <string>
+#include <vector>
 
 class codeSet
 {
    public:
         int n;
         std::string str;
+        std::string favorite;
 };
+std::vector<std::string> getBitStrings(unsigned int n)
+{
+    std::vector<std::string> result;
+
+    if (n <= 1)
+    {
+        result.push_back("0");
+        result.push_back("1");
+    }
+    else
+    {   // recurse, then add extra bit chars
+        std::vector<std::string> sub = getBitStrings(n-1);
+        for (std::vector<std::string>::const_iterator it = sub.cbegin();
+             it != sub.cend(); ++it)
+        {
+            result.push_back(*it+'0');
+            result.push_back(*it+'1');
+        }
+    }
+    return result;
+}
 
 int main()
 {
+    std::string favorite;
+    int n;
+    
     std::cin >> n;
+    std::cin >> favorite;
     if (n > 0)
     {
         for ( int z = 0; z < n; z++)
