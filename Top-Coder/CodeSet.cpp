8f719ee1f857b0481a53655845817f865fd1d586
Auriga <AG-Systems@users.noreply.github.com>
2015-11-19 10:43:35 -0800

    Create CodeSet.cpp

---
+++ Top-Coder/CodeSet.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+
+class codeSet
+{
+   public:
+        int n;
+        std::string str;
+};
+
+int main()
+{
+    std::cin >> n;
+    if (n > 0)
+    {
+        for ( int z = 0; z < n; z++)
+        {
+                
+        }
+    }
+}
