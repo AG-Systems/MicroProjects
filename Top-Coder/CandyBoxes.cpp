6d5ea9a113034c0e67f693f567b76252430dc945
Auriga <AG-Systems@users.noreply.github.com>
2015-11-20 23:52:55 -0800

    Update CandyBoxes.cpp

--- Top-Coder/CandyBoxes.cpp
+++ Top-Coder/CandyBoxes.cpp
@@ -2,14 +2,15 @@
 
 int main()
 {
-    int boxes[3] = {'7', '21', '14'};
+    int boxes [] = {'7', '21', '14'};
     int smallest = boxes[0] ;
-    for(int z = 0; z < 10; z++)
-    {
         for ( int i=1;  i < sizeof(boxes)/sizeof(boxes[0]);  ++i )
-        if ( boxes[i] < smallest )
-             smallest = boxes[i] ;
-
+        {
+            if ( boxes[i] < smallest )
+            {
+                smallest = boxes[i] ;
+            }
+        }
     std::cout << "min: " << smallest << '\n' ;           
     }
 }
