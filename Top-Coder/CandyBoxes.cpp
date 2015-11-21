51788d3625242d050eec7b05087923071dd6ce10
Auriga <AG-Systems@users.noreply.github.com>
2015-11-20 23:48:38 -0800

    Update CandyBoxes.cpp

--- Top-Coder/CandyBoxes.cpp
+++ Top-Coder/CandyBoxes.cpp
@@ -1,1 +1,15 @@
+#include <iostream>
 
+int main()
+{
+    int boxes[3] = {'7', '21', '14'}
+    
+    for(int z = 0; z < 10; z++)
+    {
+        for ( int i=1;  i < sizeof(array)/sizeof(array[0]);  ++i )
+        if ( array[i] < smallest )
+             smallest = array[i] ;
+
+    cout << smallest << '\n' ;           
+    }
+}
