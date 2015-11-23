b0b978af6f33dcfc0f2df34320858834a02e0d4e
Auriga <AG-Systems@users.noreply.github.com>
2015-11-22 23:10:10 -0800

    Update Answer.cpp

--- Top-Coder/Answer.cpp
+++ Top-Coder/Answer.cpp
@@ -1,12 +1,30 @@
 #include <iostream>
 #include <string>
+#include <array>
+#include <algorithm>
+#include <sstream>
 
 int main()
 {
     char input;
+    std::string xtr;
     int counter;
     std::string words[] = {"CAT", "DOG", "ANT"};
     std::cin >> input;
-    
+    std::stringstream ss;
+    ss << input;
+    ss >> xtr;
+    for(int z = 0; z < sizeof(words); z++)
+    {
+        for(int x = 0; x <sizeof( words[z] ); x++)
+        {
+            if (xtr != words[x])
+            {
+                //std::replace( words[z].begin(), words[z].end(), 'x', 'y');
+                std::cout << "hello " << std::endl;
+            }
+        }
+    }
+
     
 }
