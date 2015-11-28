39eda5342a26e0d7c566ded5d212cf7fcbec31a3
Auriga <AG-Systems@users.noreply.github.com>
2015-11-28 02:05:11 -0800

    Create PasswordGen.cpp

---
+++ Hacking/PasswordGen.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <string>
+#include <stdlib.h>
+#include <time.h>
+#include <vector>
+
+template <class T> class func
+{
+    public:
+    T number;
+    T length;
+    
+};
+
+int lower,upper,digi,marks,sim,length;
+const char LOWER_CASE[] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+const char UPPER_CASE[] = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
+//LOWER_CASE.toUpperCase();
+const char DIGITS[] = {'0','1','2','3','4','5','6','7','8','9'};
+// const char PUNCT_MARKS[] = {'!',',','.',':',';','@','#','$','%','^','*','(',')','_','-','+','=','{','}','\','<','>',''?,'\','№'};
+const char SIMILAR_CHARACTERS[] = {'L','l','1','i','o','O','0'};
+int main()
+{
+    srand ( time(NULL) );
+    std::vector <char> list;
+    std::vector <char> password;
+    std::cout << "Please type 1 for yes or 0 for no on the following: Lowercase : Uppercase : Digits : Puncation marks : Similar characters : Length " << std::endl;
+    std::cin >> lower,upper,digi,marks,sim,length;
+    for (int z = 0; 0 < length; z++)
+    {
+        if (lower == 1)
+        {
+            int RandIndex = rand() % 25;
+            list.push_back (LOWER_CASE[RandIndex]);
+        }
+        if (upper == 1)
+        {
+            int RandIndex = rand() % 25;
+            list.push_back (UPPER_CASE[RandIndex]);    
+        }
+        if (digi == 1)
+        {
+            int RandIndex = rand() % 9;
+            list.push_back (DIGITS[RandIndex]);     
+        }
+        if (marks == 1)
+        {
+            
+        }
+        if (sim == 1)
+        {
+            int RandIndex = rand() % 6;
+            list.push_back (SIMILAR_CHARACTERS[RandIndex]);    
+        }
+    }
+}
