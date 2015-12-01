3c22d47d399d95eeec6f895dd6771143b130868e
Auriga <AG-Systems@users.noreply.github.com>
2015-12-01 03:59:06 -0800

    Create sumy.py

---
+++ Inspirationals/PythonSummary/sumy.py
@@ -0,0 +1,25 @@
+from __future__ import absolute_import
+from __future__ import division, print_function, unicode_literals
+
+from sumy.parsers.html import HtmlParser
+from sumy.parsers.plaintext import PlaintextParser
+from sumy.nlp.tokenizers import Tokenizer
+from sumy.summarizers.lsa import LsaSummarizer as Summarizer
+from sumy.nlp.stemmers import Stemmer
+from sumy.utils import get_stop_words
+
+LANGUAGE = "english"
+SENTENCES_COUNT = 10
+#nltk.download()
+if __name__ == "__main__":
+    url = "http://money.cnn.com/2015/12/01/investing/premarket-stocks-trading/index.html?iid=hp-stack-dom"
+    parser = HtmlParser.from_url(url, Tokenizer(LANGUAGE))
+    # or for plain text files
+    # parser = PlaintextParser.from_file("document.txt", Tokenizer(LANGUAGE))
+    stemmer = Stemmer(LANGUAGE)
+
+    summarizer = Summarizer(stemmer)
+    summarizer.stop_words = get_stop_words(LANGUAGE)
+
+    for sentence in summarizer(parser.document, SENTENCES_COUNT):
+        print(sentence)
