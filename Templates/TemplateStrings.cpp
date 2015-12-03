174a571e94dabc5504ff1bc4b7fa0f62898893da
Auriga <AG-Systems@users.noreply.github.com>
2015-12-03 12:06:53 -0800

    Create TemplateStrings.cpp

---
+++ Templates/TemplateStrings.cpp
@@ -0,0 +1,24 @@
+const char AlphabetLower[26] =
+{
+	'a', 'b', 'c', 'd', 'e', 'f', 'g',
+	'h', 'i', 'j', 'k', 'l', 'm', 'n',
+	'o', 'p', 'q', 'r', 's', 't', 'u',
+	'v', 'w', 'x', 'y', 'z'
+};
+
+const char AlphabetUpper[26] =
+{
+	'A', 'B', 'C', 'D', 'E', 'F', 'G',
+	'H', 'I', 'J', 'K', 'L', 'M', 'N',
+	'O', 'P', 'Q', 'R', 'S', 'T', 'U',
+	'V', 'W', 'X', 'Y', 'Z'
+};
+
+const char LOWER_CASE[] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+
+const char UPPER_CASE[] = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
+//LOWER_CASE.toUpperCase();
+
+const char DIGITS[] = {'0','1','2','3','4','5','6','7','8','9'};
+// const char PUNCT_MARKS[] = {'!',',','.',':',';','@','#','$','%','^','*','(',')','_','-','+','=','{','}','\','<','>',''?,'\','№'};
+const char SIMILAR_CHARACTERS[] = {'L','l','1','i','o','O','0'};
