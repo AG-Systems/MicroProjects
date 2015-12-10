9d9e113032dec0bfd71bbc2dcd8a3f054e805151
Auriga <AG-Systems@users.noreply.github.com>
2015-12-10 10:42:00 -0800

    Create 1.cpp

---
+++ Class-Practice/1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+
+class Speed
+{
+    public:
+    int speed;   
+};
+
+int main()
+{
+    Speed Speed1;
+    Speed1.speed = 0;
+    
+    std::cout << Speed1.speed << std::endl;
+}
