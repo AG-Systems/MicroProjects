d114b3d2f2b1bf32ac15ac1d88f77d4e3a79c806
Auriga <AG-Systems@users.noreply.github.com>
2015-12-13 15:26:16 -0800

    Update site.py

--- temp/site.py
+++ temp/site.py
@@ -4,16 +4,13 @@ from newspaper import Article
 import time
 import nltk
 #nltk.download()
-test = newspaper.build('http://www.cnn.com/2015/12/12/entertainment/marjorie-lord-dies-feat/index.html')
-test.download()
-test.parse()
 time.sleep(10)
 #test.nlp()
 print "started"
+cnn_paper = newspaper.build('https://news.ycombinator.com/')
 n = 0
 while( -1 < n):
     try:
-        cnn_paper = newspaper.build('http://cbs.com')
         for article in cnn_paper.articles:
             print(article.url)
         time.sleep(10)
@@ -30,14 +27,17 @@ while( -1 < n):
         first_article.nlp()
         time.sleep(10)
         print "going to open"
+        time.sleep(10)
         title = first_article.title
         summary = first_article.summary
         author = first_article.authors
+        urll = first_article.url
+        slash = " \ "
         with open('blogs.json', 'r+') as f:
             data = json.load(f)
             #data['id'] = 134 # <--- add `id` value.
             f.seek(0)        # <--- should reset file position to the beginning.
-            data["blogs"].append({'header': title , 'author': author , 'team': 'Webmaster' , 'date': [], 'paragraphs' : [ summary ]})
+            data["blogs"].append({'header': title , 'author': urll , 'team': 'Webmaster' , 'date': [], 'paragraphs' : [  summary ]})
             json.dump(data, f)
         n += 1
         print "finished"
