e4066e94b23e5b0ae78dbf79b48a88857349ad0d
Auriga <AG-Systems@users.noreply.github.com>
2015-12-14 10:40:58 -0800

    Update Array Sort - Basic Merge Sort.cpp

--- Algorithm/Array Sort - Basic Merge Sort.cpp
+++ Algorithm/Array Sort - Basic Merge Sort.cpp
@@ -2,15 +2,19 @@
 #include <vector>
 #include <algorithm>
 
+
 int main()
 {
 	std::vector <int> list {1,2,3,4,5,6,7,8};
+	int n = list.size();
 	if(list.size() / 2 )
 	{
-		std::size_t const half_size = list.size() / 2;
-		std::vector<int> split_lo(list.begin(), list.begin() + half_size);
-		std::vector<int> split_hi(list.begin() + half_size, list.end());
-		
+		for(int x = 0; x < n; x++)
+		{
+			std::size_t const half_size = list.size() / 2;
+			std::vector<int> split_lo(list.begin(), list.begin() + half_size);
+			std::vector<int> split_hi(list.begin() + half_size, list.end());
+		}
 		
 	}
 }
