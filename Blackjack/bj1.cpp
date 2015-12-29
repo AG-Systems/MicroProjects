3c87eb8deafe900ade5f11e30ad791d316b1b04e
Auriga <AG-Systems@users.noreply.github.com>
2015-12-28 23:28:38 -0800

    Update bj1.cpp

--- Blackjack/bj1.cpp
+++ Blackjack/bj1.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 
+int dealer;
+int a[] = {1,2,3,4,5,6,7,8,9}
+int numcards;
 int main()
 {
-  
+  std::cout << "How many cards is there:  " << std::endl;
+  std::cin >> numcards;
+  std::cout << "What are the cards: "
 }
