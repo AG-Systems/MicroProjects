aeb90a484d97a8b1f18a1e1d25fb28efeb01fa6a
Auriga <AG-Systems@users.noreply.github.com>
2016-01-05 16:21:32 -0800

    Create updatedforce.js

---
+++ Physics/updatedforce.js
@@ -0,0 +1,62 @@
+$.fn.serializeObject = function() {
+    var o = {};
+    var a = this.serializeArray();
+    $.each(a, function() {
+        if (o[this.name] !== undefined) {
+            if (!o[this.name].push) {
+                o[this.name] = [o[this.name]];
+            }
+            o[this.name].push(this.value || '');
+        } else {
+            o[this.name] = this.value || '';
+        }
+    });
+    return o;
+};
+
+$(document).ready(function() {
+    var numOfForces = 1;
+    var MAX_FORCES = 10, MIN_FORCES = 1;
+
+    $('#add').on('click', function(e) {
+        e.preventDefault();
+        if (numOfForces < MAX_FORCES) {
+            numOfForces ++;
+            var clone = $('.force-group').first().clone();
+            clone.children().children('b').text('Force ' + numOfForces);
+            $.each(clone.children().children('input'), function(i){
+                $(this).attr('name', $(this).attr('name').slice(0, $(this).attr('name').length-1) + (numOfForces - 1));
+            });
+            $('#forceForm').append(clone);
+        };
+    });
+
+    $('#remove').on('click', function(e) {
+        if (numOfForces > MIN_FORCES) {
+            numOfForces--;
+            $('.force-group').last().remove();
+        };
+    });
+
+    $('#calculate').on('click', function(e) {
+        e.preventDefault();
+        $('#forceForm').submit();
+    });
+    var tableElements = $('#dvatChart').find('tr');
+    $('#forceForm').on('submit', function(e){
+        e.preventDefault();
+        var forceJSON = $('#forceForm').serializeObject();
+        forceJSON.numOfForces = numOfForces;
+        $.post('/force/solve', forceJSON, function(res){
+            console.log(res);
+            var mangi = res.magnitude;
+            var anglex = res.xNetForce;
+            var angley = res.yNetForce;
+            var dir = res.direction;
+         tableElements.eq(1).append('<td class="pad"> Net Force: ' + mangi + '</td>');
+         tableElements.eq(2).append('<td class="pad"> Direction: ' + dir + '</td>');;
+
+
+        });
+    });
+});
