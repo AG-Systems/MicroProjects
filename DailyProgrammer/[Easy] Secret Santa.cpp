f8847d5b3a0792eacc6a3f8d701abdc9f8250699
Auriga <AG-Systems@users.noreply.github.com>
2016-01-14 13:24:25 -0800

    Update [Easy] Secret Santa.cpp

--- DailyProgrammer/[Easy] Secret Santa.cpp
+++ DailyProgrammer/[Easy] Secret Santa.cpp
@@ -7,6 +7,8 @@ int main()
 {
   srand ( time(NULL) );
   std::string a[4] = {"Joe", "Jeff", "Jerry", "Johnson"};
+  int id[4] = {0,1,1,0};
   int RandIndex = rand() % 4; 
   std::cout << a[RandIndex] << std::endl;
+
 }
