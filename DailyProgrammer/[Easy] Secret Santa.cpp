5aee4e14f205b98ccbcce7db5c7cb7e8c1687ea0
Auriga <AG-Systems@users.noreply.github.com>
2016-01-14 13:17:39 -0800

    Create [Easy] Secret Santa.cpp

---
+++ DailyProgrammer/[Easy] Secret Santa.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+#include <stdlib.h>
+#include <time.h>
+
+
+int main()
+{
+  srand ( time(NULL) );
+  std::vector <std::string> list = {"Joe", "Jeff" ,"Jerry", "Johnson"}
+}
