96e67956715786dd6efdc8415c4aced5f0d31acb
Auriga <AG-Systems@users.noreply.github.com>
2016-01-21 13:25:06 -0800

    Update Main.js

--- GameScripts/MainMenu/Main.js
+++ GameScripts/MainMenu/Main.js
@@ -1,5 +1,6 @@
 #pragma strict
-
+var counter = 0;
+var debuff = 2;
 function Start () 
 {
 
@@ -10,12 +11,44 @@ function Update ()
 
 }
 
-function StartGame()
-{
-Application.loadedLevel();
-}
-
-function ExitGame()
-{
-
-}
+ private var showStamina : boolean = true;
+ private var showDefense : boolean = true;
+ private var showStr : boolean = true;
+ function OnGUI()
+ {
+     if(showStamina)
+     {
+         if(GUI.Button(Rect(0, 0, 128, 128), "Stamina"))
+         {
+         	if(counter < debuff )
+         	{
+         		showStamina = false;
+         		counter++;
+         	}
+         }
+     }
+     // Start defense button
+ 	if(showDefense)
+ 	{
+          if(GUI.Button(Rect(0, 150, 128, 128), "Defense"))
+         {
+         	if(counter < debuff )
+         	{
+         		showDefense = false;
+         		counter++;
+         	}
+         }
+    }
+    //end of defense button
+    if(showStr)
+ 	{
+         if(GUI.Button(Rect(0, 300, 128, 128), "Str"))
+         {
+         	if(counter < debuff )
+         	{
+         		showStr = false;
+         		counter++;
+         	}
+         }
+    }
+ }
