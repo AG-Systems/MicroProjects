2109a0f23f478c9de87dbdcc065518f4bbcfd02c
Auriga <AG-Systems@users.noreply.github.com>
2016-02-02 22:42:36 -0800

    Update Homeworkhelp2.py

--- Python/Homeworkhelp2.py
+++ Python/Homeworkhelp2.py
@@ -8,7 +8,7 @@ print(board[3] + board[4] + board[5])
 print(board[6] + board[7] + board[8])
 while(counter < maxCounter):
     location = int(input("Pick a location, 1-9: "))
-    if counter %2 == 0:
+    if counter % 2 == 0:
         board[location - 1] = "X"
     else:
         board[location - 1] = "O"    
