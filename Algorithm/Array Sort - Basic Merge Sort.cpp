46d43650f698a279a9f5eaacd6b30bfb8714e120
Auriga <AG-Systems@users.noreply.github.com>
2016-02-05 10:26:30 -0800

    Update Array Sort - Basic Merge Sort.cpp

--- Algorithm/Array Sort - Basic Merge Sort.cpp
+++ Algorithm/Array Sort - Basic Merge Sort.cpp
@@ -8,11 +8,6 @@ class list
     std::vector <int> list {1,2,3,4,5,6,7,8};
 };
 
-int fun(std::vector <int> a{1,2,3,4,5,6,7,8})
-{
-  std::vector<T> a2(a.begin(), a.begin() + a.size()/2),
-               a3(a.begin() + a.size()/2, a.end());  
-}
 int main()
 {
     
