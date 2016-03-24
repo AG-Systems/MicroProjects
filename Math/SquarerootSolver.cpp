85f32477e62d2a4891599d5d00bded18cee3e3e6
Auriga <AG-Systems@users.noreply.github.com>
2016-03-24 15:32:27 -0700

    Create SquarerootSolver.cpp

---
+++ Math/SquarerootSolver.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cmath>
+
+double result;
+
+int main()
+{
+    for(int z = 0; z < 49; z++)
+    {
+        result = sqrt (49);
+        if (z == result)
+        {
+            std::cout << "your number is " << z << std::endl;
+            break;
+        }
+    
+    } 
+}
