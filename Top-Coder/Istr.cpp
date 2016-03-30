386d084ea15ffc51c4073b152e1c850641c9852f
Auriga <AG-Systems@users.noreply.github.com>
2016-03-30 15:54:28 -0700

    Update Istr.cpp

--- Top-Coder/Istr.cpp
+++ Top-Coder/Istr.cpp
@@ -17,24 +17,19 @@ public:
 };
 int Istr::count(std::string str, int x)
 {
-	std::vector<char> list;
+	std::vectr <int> list;
 	for(int z = 0; z < 26;z++)
 	{
 		size_t n = std::count(str.begin(), str.end(), AlphabetLower[z]);
 		std::cout << AlphabetLower[z] << ": " << n << std::endl;
-		if (n > 0)
-		{
-		    for(int xx =0; xx < n; xx++)
-		    {
-		        list.push_back (AlphabetLower[z]);	
-		    }
-		}
-
+		int zz = n * n;
+		list.push_back (zz);
 	}
+	
 }
 int main()
 {
-	std::string str = "hello"; 
+	std::string str = "aba"; 
 	Istr a;
 	a.count(str, 0);
 }
