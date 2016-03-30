39ee70c0b24286d57b741fea6d95444f371267ac
Auriga <AG-Systems@users.noreply.github.com>
2016-03-30 16:31:53 -0700

    Update Istr.cpp

--- Top-Coder/Istr.cpp
+++ Top-Coder/Istr.cpp
@@ -18,10 +18,17 @@ public:
 int Istr::count(std::string str, int x)
 {
 	std::vector <int> list;
+	std::vector <int> finallist;
+	char max;
+	int maxvalue = 0;
 	for(int z = 0; z < 26;z++)
 	{
 		size_t n = std::count(str.begin(), str.end(), AlphabetLower[z]);
 		std::cout << AlphabetLower[z] << ": " << n << std::endl;
+		if(n > maxvalue)
+		{
+		    max == AlphabetLower[z];    
+		}
 		int zz = n * n;
 		list.push_back (zz);
 	}
@@ -35,7 +42,27 @@ int Istr::count(std::string str, int x)
 	    std::cout << "Value of the string is: " << counter << std::endl;
 	    return counter;
 	}
-	
+	if(x != 0)
+	{
+	    for(int i = 0; i < 26; i++)
+	    {
+	        if(AlphabetLower[i] == max)
+	        {
+	            for(int z = 0; z < x; z++)
+	            {
+	                list.erase(i + z);    
+	            }
+	            
+	        }
+	    }
+	    int counter = 0;
+	    for(int i = 0; i < 26; i++)
+	    {
+	        counter += list[i];        
+	    }
+	    std::cout << "Value of the string is: " << counter << std::endl;
+	    return counter;
+	}
 }
 int main()
 {
