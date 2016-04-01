895adc36c4b91e410a4e65d9e6bd3e559f0f4dd6
Auriga <AG-Systems@users.noreply.github.com>
2016-03-31 19:20:42 -0700

    Update and rename Traffic.cpp to traffic.cpp

--- Top-Coder/traffic.cpp
+++ Top-Coder/traffic.cpp
@@ -9,21 +9,19 @@ public:
 int traffic::time(int lights[], int speed)
 {
 	// each light is 150 meters.
+	int totaldistance = sizeof(lights) * 150;
 	int timecounter = 0;
 	int distance = 0;
-	int iter = 0;
 	while(true)
 	{
 		timecounter++;
 		distance += speed;
-		if(distance >= 150 && timecounter < lights[iter])
-		{
-	
-		}
-
 	}
 }
 int main()
 {
-	
+	int array[] = {10,10,10};
+	int speed = 10;
+	traffic main;
+	main.time(array,speed);
 }
