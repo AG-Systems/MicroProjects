84e1718a6ca1d7ded6eb2cbb791563cc29cf72c4
Auriga <AG-Systems@users.noreply.github.com>
2016-04-16 15:49:18 -0700

    Create Array Search - Linear Search.cpp

---
+++ Algorithm/Array Search - Linear Search.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <iterator>
+#include <type_traits>
+
+void linearsearch(int key, int a[],int arraysize)
+{
+	for(int z = 0; z < arraysize; z++)
+	{
+		if(key == a[z])
+		{
+			std::cout << "Found. The position is: " << z << std::endl;
+			break;
+		}
+	}
+}
+
+int main()
+{
+	int array[] = {1,2,3,4,5,6,7,8};
+	linearsearch(5,array, 8);
+	std::cin.get();
+}
