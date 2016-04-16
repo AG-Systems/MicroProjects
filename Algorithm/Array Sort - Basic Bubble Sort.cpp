05985d91437659a44feca52fd3f3785909f70b53
Auriga <AG-Systems@users.noreply.github.com>
2016-04-15 18:38:37 -0700

    Update Array Sort - Basic Bubble Sort.cpp

--- Algorithm/Array Sort - Basic Bubble Sort.cpp
+++ Algorithm/Array Sort - Basic Bubble Sort.cpp
@@ -12,20 +12,20 @@ void bubblesort(int a[], int size)
         for(int z = 0; z < size; z++)
         {
             std::cout << a[z] << " ";
-			if(a[z] < a[z+1] && a[z] != a[size])
-            {
-                checker++;        
-            }
-			if(a[z] > a[z+1] && a[z] != a[size])
-            {
-                std::swap(a[z],a[z+1]);
-                std::cout << a[z] << " ";
-            }
-            if(checker == size)
-            {
-                win = false;
-                break;
-            }
+		if(a[z] < a[z+1] && a[z] != a[size])
+            	{
+                	checker++;        
+            	}
+		if(a[z] > a[z+1] && a[z] != a[size])
+            	{
+                	std::swap(a[z],a[z+1]);
+                	std::cout << a[z] << " ";
+            	}
+            	if(checker == size)
+            	{
+                	win = false;
+                	break;
+            	}
         }
     }
     std::cout << "\n" << std::endl;
@@ -39,5 +39,5 @@ int main()
 {
     int a[] = {8,1,5,4,3,6,7,2};
     bubblesort(a,7); 
-	std::cin.get();
+    std::cin.get();
 }
