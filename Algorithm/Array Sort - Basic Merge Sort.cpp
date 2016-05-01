0b334063e64ba1db9ceffb79819d941574813267
Auriga <AG-Systems@users.noreply.github.com>
2016-05-01 15:49:17 -0700

    Update Array Sort - Basic Merge Sort.cpp

--- Algorithm/Array Sort - Basic Merge Sort.cpp
+++ Algorithm/Array Sort - Basic Merge Sort.cpp
@@ -1,14 +1,35 @@
 #include <iostream>
 #include <vector>
 #include <algorithm> 
+#include <iterator>
 
-class list
+void merge(std::vector <int> v1,std::vector <int> v2, std::vector <int> v3)
 {
-    public:
-    std::vector <int> list {1,2,3,4,5,6,7,8};
-};
+	std::vector <int> finallist;
+	std::vector<int> v2(v1.begin(), v1.begin() + v1.size()/2),
+					 v3(v1.begin() + v1.size()/2, v1.end());
+	if(v2.size() == 0 || v3.size() == 0)
+	{
+		if(v2[0] < v3[0])
+		{
+			finallist.push_back (v2[0]);
+			finallist.push_back (v3[0]);
+		}
+		else
+		{
+			finallist.push_back (v3[0]);
+			finallist.push_back (v2[0]);	
+		}
+		return;
+	}
+	else
+	{
+		merge(v2, v3, v3);
+	}
+}
 
 int main()
 {
-    
+
+
 }
