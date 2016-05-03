2db58a388e3c6f4df266502d49da5608fab641f2
Auriga <AG-Systems@users.noreply.github.com>
2016-05-02 19:55:10 -0700

    Create Array Search - Binary Search.cpp

---
+++ Algorithm/Array Search - Binary Search.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+
+std::vector <int> binarysearch(std::vector <int> list,int target)
+{
+    int half = list.size() / 2;
+    if (list.size() <= 2)
+    {
+        for(int c = 0; c < 2; c++)
+        {
+            if(list[c] == target)
+            {
+                std::cout << "Found" << std::endl;
+                return list;    
+            }
+        }
+    }
+    if(list.size() % 2 == 0 && list.size() > 2)
+    {
+        if(list[half] == target)
+        {
+            std::cout << "Found number in this position: " << half << std::endl;    
+        }
+        if(list[half] > target)
+        {
+            for(int z = 0; z < target; z++)
+            {
+                list.erase(list.begin() + z);    
+            }
+        }
+        if (list[half] < target)
+        {
+            int half = list.size() / 2;
+            for(int z = list.size(); z > target; z--)
+            {
+                 list.pop_back();                    
+            }
+        }
+    }
+    binarysearch(list,target);
+}
+
+int main()
+{
+        
+}
