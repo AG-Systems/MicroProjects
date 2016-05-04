af94803c7dde74008abd5263331b9a660c59e5a9
Auriga <AG-Systems@users.noreply.github.com>
2016-05-04 12:20:00 -0700

    Update Array Sort - Quick Sort.cpp

--- Algorithm/Array Sort - Quick Sort.cpp
+++ Algorithm/Array Sort - Quick Sort.cpp
@@ -68,5 +68,16 @@ void quicksort(std::vector <int> list,std::vector <int> emptylist)
 
 int main()
 {
-    
+    std::vector<int> myvector;
+    std::vector<int> sortedvector;
+	int a[8] = {0, 1, 2, 3, 4, 5, 6, 7};
+    std::srand(unsigned(time(0)));
+    std::random_shuffle(a, a + 8);
+	for (int i=0; i<8; i++)
+    {
+        std::cout << a[i] << " ";
+        myvector.push_back (a[i]);
+    }   
+    std::cout << "\n" << std::endl;
+    quicksort(myvector,sortedvector);
 }
