09f6a77403bb5872d862b628737ac234bbeaec04
Auriga <AG-Systems@users.noreply.github.com>
2016-05-05 19:05:19 -0700

    Update Array Sort - Insertion Sort.cpp

--- Algorithm/Array Sort - Insertion Sort.cpp
+++ Algorithm/Array Sort - Insertion Sort.cpp
@@ -1,26 +1,41 @@
 #include <iostream>
 #include <vector>
+#include <ctime>
+#include <cstdlib>
+#include <algorithm>
 
-void insertionsort(std::vector <int> list)
+void insertionsort(std::vector <int> a)
 {
-	int wall = list[0];
-	for(int z = 1; z < list.size(); z++)
+	for(int z = 0; z < a.size()-1; z++)
 	{
-		if(list[z] > list[z-1])
+		int next = z + 1;
+		int temp = a[next];
+		while(next > 0 && temp > a[next-1])
 		{
-			for(int x = z; x > 0; x--)
-			{
-				if(list[x] > list[x+1])
-				{
-					list.insert(list.begin()+x, list.begin()+x+1);
-				}
-			}
+			a[next] = a[next-1];
+			next--;
 		}
+		a[next] = temp;
 	}
+	for(int c = 0; c < a.size();c++)
+	{
+	    std::cout << a[c] << " ";        
+	}
+    
 }
-
 
 int main()
 {
-
+    std::vector<int> myvector;
+    std::vector<int> sortedvector;
+	int a[8] = {0, 1, 2, 3, 4, 5, 6, 7};
+    std::srand(unsigned(time(0)));
+    std::random_shuffle(a, a + 8);
+	for (int i=0; i<8; i++)
+    {
+        std::cout << a[i] << " ";
+        myvector.push_back (a[i]);
+    }
+	std::cout << "\n";
+    insertionsort(myvector);
 }
