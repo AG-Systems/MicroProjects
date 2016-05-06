aade643c86823be285731388ca36cd21ef3af383
Auriga <AG-Systems@users.noreply.github.com>
2016-05-05 18:58:29 -0700

    Update Array Sort - Insertion Sort.cpp

--- Algorithm/Array Sort - Insertion Sort.cpp
+++ Algorithm/Array Sort - Insertion Sort.cpp
@@ -1,1 +1,26 @@
+#include <iostream>
+#include <vector>
 
+void insertionsort(std::vector <int> list)
+{
+	int wall = list[0];
+	for(int z = 1; z < list.size(); z++)
+	{
+		if(list[z] > list[z-1])
+		{
+			for(int x = z; x > 0; x--)
+			{
+				if(list[x] > list[x+1])
+				{
+					list.insert(list.begin()+x, list.begin()+x+1);
+				}
+			}
+		}
+	}
+}
+
+
+int main()
+{
+
+}
