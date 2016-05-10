fe86b2f94bc19756e8374533d23a662cf2ad27e6
Auriga <AG-Systems@users.noreply.github.com>
2016-05-10 13:03:44 -0700

    Copied code. Must make my own.

--- DataStructures/Linkedlist.cpp
+++ DataStructures/Linkedlist.cpp
@@ -29,6 +29,94 @@ struct node *searchNode(int n)
 		cur = cur->next;
 	}
 }
+struct node *searchNode2(int n) 
+{
+	if(head == NULL) 
+	{
+	    return head;
+	}
+	node *cur = head;
+	node *prev = head;
+	while(cur) 
+	{
+		if(cur->data == n)
+		{
+		    return cur;
+		}
+		prev = cur;
+		cur = cur->next;
+	}
+	return cur;
+}
+void addNode(int n) 
+{
+	node *newNode = new node;
+	newNode->data = n;
+	newNode->next = NULL;
+
+	if(head == NULL) 
+	{
+		head = newNode;
+		return;
+	}
+	node *cur = head;
+	while(cur) {
+		if(cur->next == NULL) 
+		{
+			cur->next = newNode;
+			return;
+		}
+		cur = cur->next;
+	}
+}
+void insertNode(int n) 
+{
+	node *ptr = searchNode(n);
+	node *newNode = new node;
+	newNode->data = n;
+	node *cur = head;
+	while(cur) 
+	{
+		if(cur == ptr ) 
+		{
+			newNode->next = cur->next;
+			cur->next = newNode;
+			return;
+		}
+		cur = cur->next;
+	}
+}
+
+void deleteNode(int n) {
+	node *ptr = searchNode(n);
+	if(ptr == NULL) 
+		std::cout << "No node with data = " << n << std::endl;
+	if(ptr == head) {
+		head = ptr->next;
+		return;
+	}
+	node *cur = head;
+	node *prev = head;
+
+	while(cur) {
+		if(cur == ptr) {
+			prev->next = cur->next;
+			return;
+		}
+		prev = cur;
+		cur = cur->next;
+	}
+}
+
+void display() 
+{
+	struct node *list = head;
+	while(list) {
+		std::cout << list->data <<" ";
+		list = list->next;
+	}
+	std::cout << "\n";
+}
 int main()
 {
         
