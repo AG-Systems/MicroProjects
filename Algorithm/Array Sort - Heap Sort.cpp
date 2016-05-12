07cc35c93dbeec28dc8d8673091ce21feb28b141
Auriga <AG-Systems@users.noreply.github.com>
2016-05-12 12:26:44 -0700

    Update Array Sort - Heap Sort.cpp

--- Algorithm/Array Sort - Heap Sort.cpp
+++ Algorithm/Array Sort - Heap Sort.cpp
@@ -23,3 +23,31 @@ void heapify(std::vector <int> list, int nodesize, int node)
         heapify(list,nodesize,largest);
     }
 }
+
+void heap_sort(std::vector <int> list, int nodesize)
+{
+    for(int z = nodesize / 2 - 1; z >= 0; z--)
+    {
+        heapify(list,nodesize,z);    
+    }
+    for(int z = nodesize-1; z >= 0; z--)
+    {
+        std::swap(list[0], list[z]);
+        heapify(list,z,0);
+    }
+}
+void convertvector(int a[], int size,std::vector <int> newlist)
+{
+    for(int x = 0; x < size; x++)
+    {
+        newlist.push_back (a[x]);    
+    }
+}
+int main()
+{
+    std::vector <int> newlist;
+    int a[] = {12, 11, 13, 5, 6, 7};  
+    convertvector(a,6,newlist);
+    
+    heap_sort(newlist,newlist.size());
+}
