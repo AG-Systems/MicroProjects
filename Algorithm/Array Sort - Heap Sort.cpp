846384a603513893fad58b558044c6ef3f8113b6
Auriga <AG-Systems@users.noreply.github.com>
2016-05-12 08:35:09 -0700

    Update Array Sort - Heap Sort.cpp

--- Algorithm/Array Sort - Heap Sort.cpp
+++ Algorithm/Array Sort - Heap Sort.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
 
+// need to make a min heap
+
 void heapsort()
-{}
+{
+  // make the lowest number go to the top of the tree. Make it the parent
+  
+  // Delete the main node afterwards as you insert it into the first array slot
+  
+  // rinse and repeat
+}
 
 int main()
 {
