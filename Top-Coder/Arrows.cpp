7b45bfd39bbff7280f46a22debffe7794585fe36
Auriga <AG-Systems@users.noreply.github.com>
2016-05-15 22:14:08 -0700

    Update Arrows.cpp

--- Top-Coder/Arrows.cpp
+++ Top-Coder/Arrows.cpp
@@ -1,1 +1,28 @@
+#include <iostream>
 
+class Arrows
+{
+public:
+	int longestArrow(std::string);
+};
+
+int Arrows::longestArrow(std::string str)
+{
+	int max = 0;
+	for(int z = 0; z < str.size(); z++)
+	{
+		int counter = 0;
+		if(str[z] == '<')
+		{
+			if(str[z+1] == '-' || str[z+1] == '=')
+			{
+				counter++;
+			}
+		}
+	}
+}
+
+int main()
+{
+
+}
