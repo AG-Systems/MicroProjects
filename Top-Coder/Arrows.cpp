5c5371e8060afc3ea46689bdc0d87e20fd4f21ae
Auriga <AG-Systems@users.noreply.github.com>
2016-05-15 23:32:46 -0700

    Update Arrows.cpp

--- Top-Coder/Arrows.cpp
+++ Top-Coder/Arrows.cpp
@@ -18,7 +18,7 @@ int Arrows::longestArrow(std::string str)
 			{
 				counter = 0;
 			}
-			else if(str[z+1] == '-' && str[z+2] == '-')
+			else if(str[z+1] ==  str[z+2])
 			{
 				counter += 2;				
 			}
