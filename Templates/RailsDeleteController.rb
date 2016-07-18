337f575dcb0ace6e533901082ccc6777cc5ee8a7
Auriga <AG-Systems@users.noreply.github.com>
2016-07-18 14:29:51 -0700

    Create RailsDeleteController.rb

---
+++ Templates/RailsDeleteController.rb
@@ -0,0 +1,9 @@
+rails destroy controller lalala
+rails destroy model yadayada
+rails destroy scaffold hohoho
+
+Rails 3.2 adds a new d shortcut to the command, so now you can write:
+
+rails d controller lalala
+rails d model yadayada
+rails d scaffold hohoho
