0da59cc3eff1e7b8ac8ce12d17c3277886d05e24
Auriga <AG-Systems@users.noreply.github.com>
2016-08-14 23:23:22 -0700

    Update NimGame.cpp

--- leetcode/NimGame.cpp
+++ leetcode/NimGame.cpp
@@ -2,22 +2,14 @@ class Solution {
 public:
     bool canWinNim(int n) 
     {
-        int counter = 0;
-        for(int x = 1; x < 3; x++)
-        {
-            int nsx = n-x;
-            if(nsx < 4 && nsx > 0)
-            {
-                counter++;
-            }
-        }
-        if(counter == 2)
-        {
+        if(n % 4 == 0) 
+        {   
             return false;
         }
         else
-        {
-            return true;
+        {    
+            return true;  
         }
-    }
+            
+        }
 };
