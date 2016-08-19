716dcb8be687a222cc6cd0c64d933e8aeee67115
Auriga <AG-Systems@users.noreply.github.com>
2016-08-18 18:59:45 -0700

    Create ContainsDuplicate.cpp

---
+++ Inspirationals/ContainsDuplicate.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_map<int, int> m;
+        for (int i = 0; i < nums.size(); ++i) {
+            if (m.find(nums[i]) != m.end()) return true;
+            ++m[nums[i]];
+        }
+        return false;
+    }
+};
