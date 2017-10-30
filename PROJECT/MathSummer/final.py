b6b01df50d50fc13af07b7648c6b91decd1d3ea9
Auriga <AG-Systems@users.noreply.github.com>
2017-10-30 15:57:37 -0700

    Create final.py

---
+++ PROJECT/MathSummer/final.py
@@ -0,0 +1,25 @@
+from Tkinter import *
+import tkFileDialog as filedialog
+import os
+import subprocess
+
+root = Tk()
+root.fileName = filedialog.askopenfilename( filetypes = ( ("Normal files", "*.txt*"),("All files", "*.*")))
+
+
+
+print (root.fileName)
+
+total_sum = 0
+with open(root.fileName, "rb") as imageFile:
+    searchfile = imageFile
+    for line in searchfile:
+        if "line length =" in line:
+            print line
+            num = float(line[line.index("=")+2:-1])
+            print("Number obtained:" + str(num))
+            total_sum += num
+            print("Sum so far: " + str(total_sum))
+    searchfile.close()
+
+print("TOTAL SUMMED B: " + str(total_sum))
